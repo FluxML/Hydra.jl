911eb2c64fa654622a8b14a0a2f98817f374715b
Maaarcocr <marcoconcettorudilosso@gmail.com>
2018-08-20 19:40:06 +0100

    hacky spmd on scalars using + only

--- src/SPMD.jl
+++ src/SPMD.jl
@@ -2,13 +2,13 @@ __precompile__(false)
 
 module SPMD
 
-using MacroTools
-using MacroTools: @forward
+  using MacroTools
+  using MacroTools: @forward
 
-include("vec.jl")
+  include("vec.jl")
 
-include("tools/ir.jl")
-include("tools/slots.jl")
-include("tools/reflection.jl")
+  include("tools/ir.jl")
+  include("tools/slots.jl")
+  include("tools/reflection.jl")
 
-end # module
+end
--- src/vec.jl
+++ src/vec.jl
@@ -4,8 +4,24 @@ struct Vec{T,N}
   data::NTuple{N,VecElement{T}}
 end
 
-vec(xs::T...) where T = Vec(VecElement.(xs))
-vec(xs...) = vec(promote(xs...)...)
+
+vect(xs::T...) where T <: Number = Vec(VecElement.(xs))
+vect(xs...) = vect(promote(xs...)...)
+
+tovect(n, x) = vect(repeat([x], n)...)
+isvect(x::Vec{T,N}) where {T,N} = true
+isvect(x) = false
+
+
+
+function Base.:+(x::Vec{T,N}, y::Vec{T,N}) where {T,N}
+  result = []
+  for i in range(1,length=N)
+    append!(result, [x.data[i].value + y.data[i].value])
+  end
+  vect(result...)
+end
+vect(1,2,3) + vect(2,3,4)
 
 struct BitVec{N,T<:Unsigned} <: AbstractVec{Bool,N}
   data::T
@@ -22,9 +38,13 @@ BitVec{N}(data::T) where {T<:Unsigned,N} = BitVec{N,T}(data)
 end
 
 # TODO use smaller words where possible
-vec(xs::Bool...) = BitVec{length(xs)}(bitpack(UInt64, xs))
+vect(xs::Bool...) = BitVec{length(xs)}(bitpack(UInt64, xs))
 
-# vec(1,2,3,4)
-# vec(true, false, true, true)
+import Base.convert, Base.promote_rule
+convert(::Type{Vec{T, N}}, x::T) where {N, T} = tovect(N,x)
+promote_rule(::Type{Vec{S,N}}, ::Type{T}) where {S,T,N} = Vec{promote_type(T,S),N}
+
+# vect(1,2,3,4)
+# vect(true, false, true, true)
 
 # spmd(::typeof(+), mask, a::Vec, b::Vec) = ...
